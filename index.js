f854f635da928e3b3f078d5b3e248b9f016a87aa
wedgwood <wedgwood@qq.com>
2017-11-09 17:02:51 +0800

    Fix coding style

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
-'use strict';
+'use strict'
 
-const fp = require('fastify-plugin');
-const mysql = require('mysql');
+const fp = require('fastify-plugin')
+const mysql = require('mysql')
 
-function fastifyMysql(fastify, options, next) {
-  const name = options.name;
-  delete options.name;
+function fastifyMysql (fastify, options, next) {
+  const name = options.name
+  delete options.name
 
-  const pool = mysql.createPool(options.connectionString || options);
+  const pool = mysql.createPool(options.connectionString || options)
   const db = {
     connect: onConnect => pool.getConnection(onConnect),
     pool: pool,
     query: pool.query.bind(pool)
-  };
+  }
 
   if (name) {
     if (!fastify.mysql) {
-      fastify.decorate('mysql', {});
+      fastify.decorate('mysql', {})
     }
 
-    fastify.mysql[name] = db;
+    fastify.mysql[name] = db
   } else {
     if (fastify.mysql) {
-      next(new Error('fastify-mysql has already registered'));
+      next(new Error('fastify-mysql has already registered'))
     } else {
-      fastify.mysql = db;
+      fastify.mysql = db
     }
   }
 
-  fastify.addHook('onClose', (fastify, done) => pool.end(done));
-  next();
+  fastify.addHook('onClose', (fastify, done) => pool.end(done))
+  next()
 }
 
-module.exports = fp(fastifyMysql, '>=0.13.1');
+module.exports = fp(fastifyMysql, '>=0.13.1')
