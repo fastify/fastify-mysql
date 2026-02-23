f3e497b1ac3c3a7e9704e70655228c4b6cd5b135
Tony133 <a.tripodi133@gmail.com>
2026-02-23 14:45:54 +0100

    chore: update plugin

--- types/index.test-d.ts
+++ types/index.test-d.ts
@@ -13,7 +13,7 @@ import fastifyMysql, {
   isMySQLPool,
 } from '..'
 import { expectType } from 'tsd'
-import { Pool } from 'mysql2/typings/mysql/lib/Pool'
+import type { Pool } from 'mysql2'
 import { Connection, PoolConnection } from 'mysql2'
 import { Pool as PromisePool, Connection as PromiseConnection, PoolConnection as PromisePoolConnection } from 'mysql2/promise'
 
